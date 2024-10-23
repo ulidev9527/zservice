4490ea78d1256bb4c80ffdb61fd496d23ccf85de
Yer <a@b.c>
2024-10-23 17:28:18 +0800

    cors add C2S_Token

--- zserviceex/ginservice/gin.middleware.cors.go
+++ zserviceex/ginservice/gin.middleware.cors.go
@@ -17,7 +17,7 @@ func GinMiddlewareCORS(zs *zservice.ZService) gin.HandlerFunc {
 		if origin != "" {
 			ctx.Header("Access-Control-Allow-Origin", "*") // 可将将 * 替换为指定的域名
 			ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-			ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+			ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,"+zservice.S_C2S_Token)
 			ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
 			ctx.Header("Access-Control-Allow-Credentials", "true")
 		}
